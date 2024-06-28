944c75b475eb7448845f078c4a8be1810b070bcb
Marcela Billingslea Durini <marcebd@meta.com>
2024-06-28 11:03:48 -0700

    new prisma schema created and migrated

--- Backend/src/loaders/routes.js
+++
@@ -1,7 +0,0 @@
-import authModule from '../components/auth/auth.module.js';
-import userModule from '../components/user/user.module.js';
-
-export default(app) => {
-    app.use('/users', userModule.router);
-    app.use('/auth', authModule.router);
-};
